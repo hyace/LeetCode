d3f179f7210098fec230a20720b34c43036bb8ea
Cheung Hui <hyace@126.com>
2014-07-12 13:27:27 +0800

    add
    Problem: Two sum

---
+++ TwoSum/Solution.java
@@ -0,0 +1,28 @@
+/**
+ * Problem: Two Sum
+ * Description: Given an array of integers, find two numbers such that they add
+ * up to a specific target number.The function twoSum should return indices of
+ * the two numbers such that they add up to the target, where index1 must be
+ * less than index2. Please note that your returned answers (both index1 and
+ * index2) are not zero-based. 
+ * Url:https://oj.leetcode.com/problems/two-sum/
+ */
+public class Solution {
+    public int[] twoSum(int[] num, int target) {
+        Map<Integer, Integer> hm = new HashMap<Integer, Integer>();
+        int N = num.length;
+        int[] result = null;
+        for (int i = 0; i < N; i++)
+            hm.put(num[i], i);
+        for (int i : num) {
+            int t = target - i;
+            if (hm.containsKey(t)) {
+                result = new int[2];
+                result[0] = Math.min(t, hm.get(t)) + 1;
+                result[1] = Math.max(t, hm.get(t)) + 1;
+            }
+        }
+        return result;
+    }
+}
+
---
+++ TwoSum/solution.py
@@ -0,0 +1,31 @@
+/**
+ * Problem: Two Sum
+ * Description: Given an array of integers, find two numbers such that they add
+ * up to a specific target number.The function twoSum should return indices of
+ * the two numbers such that they add up to the target, where index1 must be
+ * less than index2. Please note that your returned answers (both index1 and
+ * index2) are not zero-based. 
+ * Url:https://oj.leetcode.com/problems/two-sum/
+ */
+class Solution:
+    def twosum(self, num, target):
+
+        """
+        :rtype : object
+        :param num:
+        :param target:
+        :return:
+        """
+
+        n = len(num)
+        mapping = {}
+        for i in range(n):
+            mapping[num[i]] = i
+        result = [0, 0]
+        for i in range(n):
+            t = target - num[i]
+            if t in mapping:
+                result[0] = min(i + 1, mapping[t] + 1)
+                result[1] = max(i + 1, mapping[t] + 1)
+        return result
+

eabc13393980116279c42f4546e40f24ffe942ac
Cheung Hui <hyace@126.com>
2014-07-14 19:08:23 +0800

    update
    update wrong python code.

--- SearchInRotatedSortedArray/solution.py
+++ SearchInRotatedSortedArray/solution.py
@@ -6,19 +6,14 @@ class Solution:
             mid = (left + right) / 2
             if A[mid] == target:
                 return mid
-            if A[left] >= A[mid]:
-                if target < A[mid] or target >= A[left]:
+            if A[left] < A[mid]:
+                if A[left] <= target < A[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
-            elif A[mid] >= A[right]:
-                if target > A[mid] or target <= A[right]:
-                    left = mid + 1
-                else:
-                    right = mid - 1
             else:
-                if target < A[mid]:
-                    right = mid - 1
-                else:
+                if A[mid] < target <= A[right]:
                     left = mid + 1
+                else:
+                    right = mid - 1
         return -1

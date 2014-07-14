e93a9a2d89c16d721e45d0cdb01e51f81fa2b444
Cheung Hui <hyace@126.com>
2014-07-14 19:02:58 +0800

    update
    update the privious wrong code.

--- SearchInRotatedSortedArray/Solution.java
+++ SearchInRotatedSortedArray/Solution.java
@@ -17,20 +17,17 @@ public class Solution {
             int mid = (left + right) / 2;
             if (target == A[mid])
                 return mid;
-            if (A[mid] <= A[left]) {
-                if (target < A[mid] || target >= A[left])
+            if (A[mid] > A[left]) {
+                if (target < A[mid] && target >= A[left])
                     right = mid - 1;
                 else
                     left = mid + 1;
-            } else if (A[mid] >= A[right]) {
-                if (target > A[mid] || target <= A[right])
+            } else {
+                if (target > A[mid] && target <= A[right])
                     left = mid + 1;
                 else
                     right = mid - 1;
-            } else if (target < A[mid])
-                right = mid - 1;
-            else
-                left = mid + 1;
+            }
         }
         return -1;
     }

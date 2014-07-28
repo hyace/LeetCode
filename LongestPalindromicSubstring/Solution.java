498f63b21cdd332e7ceec993ff6af6e88c1dcda7
Cheung Hui <hyace@126.com>
2014-07-28 12:00:19 +0800

    Add
    Add another solution.

--- LongestPalindromicSubstring/Solution.java
+++ LongestPalindromicSubstring/Solution.java
@@ -35,4 +35,28 @@ public class Solution {
         }
         return s.substring(L, R + 1);
     }
+
+    public String longestPalindromeII(String s) {
+        int N = s.length();
+        int[] dp = new int[2 * N + 1];
+        int id = 0, mx = 0;
+        for (int i = 0; i < 2 * N + 1; ++i) {
+            int j = 2 * id - i;
+            dp[i] = mx > i ? Math.min(dp[j], mx - i) : 1;
+            int left = i - dp[i], right = i + dp[i];
+            for (; left >= 0 && right <= 2 * N; left--, right++) {
+                if (left % 2 == 0 || s.charAt(left / 2) == s.charAt(right / 2)) dp[i]++;
+                else break;
+            }
+            if (i + dp[i] > mx) {
+                id = i;
+                mx = id + dp[id];
+            }
+        }
+        int res = 0;
+        for (int i = 1; i < 2 * N + 1; ++i)
+            if (dp[i] > dp[res]) res = i;
+        int start = res / 2 - (dp[res] - 1) / 2;
+        return s.substring(start, start + dp[res] - 1);
+    }
 }

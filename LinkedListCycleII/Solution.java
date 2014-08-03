e37bf4fde4576defda0051ec0ac9f9c851add3d1
Cheung Hui <hyace@126.com>
2014-08-03 17:20:39 +0800

    Add
    Add a solution to linked list cycle II .

---
+++ LinkedListCycleII/Solution.java
@@ -0,0 +1,37 @@
+package leetcode.linkedListCycleII;
+
+import com.hyace.test.ListNode;
+
+/**
+ * Problem: Linked List Cycle
+ * Description: Given a linked list, return the node where the cycle begins. If
+ * there is no cycle, return null.
+ * Follow up: Can you solve it without using extra space?
+ * Date: Aug 2 , 2014
+ * 
+ * @author Chyace
+ * 
+ */
+public class Solution {
+    /**
+     * 设置两个指针，一个快一个慢，快的追上慢的，说明有环。
+     * 
+     * @param head
+     * @return
+     */
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) return head;
+        ListNode q = head, s = head;
+        do {
+            q = q.next.next;
+            s = s.next;
+        } while (q != null && q.next != null && q != s);
+        if (q != s) return null;
+        q = head;
+        while (q != s) {
+            q = q.next;
+            s = s.next;
+        }
+        return s;
+    }
+}

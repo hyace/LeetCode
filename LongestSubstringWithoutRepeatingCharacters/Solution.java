49f388046f4120e0ce6a904d0c3bdc090e3f4547
Cheung Hui <hyace@126.com>
2014-08-11 09:13:58 +0800

    Update
    Refactor the solution to longest substring without repeating characters.

--- LongestSubstringWithoutRepeatingCharacters/Solution.java
+++ LongestSubstringWithoutRepeatingCharacters/Solution.java
@@ -15,31 +15,46 @@ import org.junit.Test;
  */
 public class Solution {
     /**
-     * 利用bit操作来判断是否有重复字符
+     * 原以为考察的字符只是字母，结果出来一大串各种符号，所以bit操作的算法不适用了
+     * 
      * @param s
      * @return
      */
     public int lengthOfLongestSubstring(String s) {
         if (s == null || s.length() == 0) return 0;
-        int count = 0, bit = 0, res = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int ch = 1 << (s.charAt(i) - 'a');
-            if ((bit & ch) == 0) {
-                bit |= ch;
-                count++;
-            } else {
-                bit = 0;
-                count = 0;
-            }
-            res = Math.max(res, count);
+        int res = 1, start = 0;
+        for (int i = 1; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            int last = s.substring(0, i).lastIndexOf(ch);
+            if (last < start) {
+                res = Math.max(res, i - start + 1);
+            } else start = last + 1;
         }
         return res;
     }
 
+    // public int lengthOfLongestSubstring(String s) {
+    // if (s == null || s.length() == 0) return 0;
+    // int count = 0, bit = 0, res = 0;
+    // for (int i = 0; i < s.length(); i++) {
+    // int ch = 1 << (s.charAt(i) - 'a');
+    // if ((bit & ch) == 0) {
+    // bit |= ch;
+    // count++;
+    // } else {
+    // bit = 0;
+    // count = 0;
+    // i = s.substring(0, i).lastIndexOf(s.charAt(i)) + 1;
+    // }
+    // res = Math.max(res, count);
+    // }
+    // return res;
+    // }
+
     @Test
     public void test() {
-        String s = "bbbbbb";
-//        String s = "abcabcbb";
+        String s = "wlrbbmqbhcdarzowkkyhiddqscdxrjmowfrxsjybldbefsarcbynecdyggxxpklorellnmpapqfwkhopkmco";
+        // String s = "acbbabcdebb";
         System.out.println(lengthOfLongestSubstring(s));
     }
 }
